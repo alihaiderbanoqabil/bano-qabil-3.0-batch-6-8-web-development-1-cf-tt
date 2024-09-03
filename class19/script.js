7ce2f1404bcf5dc2ead1d0023794d1f9e1658f51
alihaiderbanqabil <alihaiderbanqabil2.0@gmail.com>
2024-09-03 20:10:38 +0500

    done with class 19 and class 2 of js

---
+++ class19/script.js
@@ -0,0 +1,171 @@
+// console.log("Class 2 of JS");
+// // var name;
+// // name= "ali"
+// console.log(fullName, 'fullName');
+// var fullName = "ali";
+// // var fullName = "ali";
+// fullName = "hasnain";
+// console.log(fullName, 'fullName');
+// // console.log(var1, 'var1');
+// let var1 = "var1";
+// // let var1 = "var1";
+// var1 = "var2"
+// console.log(var1, 'var1');
+
+// // console.log(var3, 'var3');
+
+
+const var3 = "var3";
+// const var3 = "var3";
+// var3 = false;
+
+// console.log(var3);
+
+let x = 45, y = "ali", z = false;    // Statement 1
+// let x;
+// let y;
+// let z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+
+// typeof
+// Class 2 of JS
+//  undefined 'fullName'
+//  hasnain fullName
+//  var2 var1
+// typeof ["ali"]
+// 'object'
+// typeof ["ali", 'hasinain']
+// 'object'
+// typeof window
+// 'object'
+// let abc;
+// undefined
+// typeof abc
+// 'undefined'
+
+
+// type conversion
+
+// Number("10")
+// 10
+// typeof Number("10")
+// 'number'
+// parseInt("10")
+// 10
+// parseInt("10.67")
+// 10
+// parseFloat("10.67")
+// 10.67
+// typeof parseFloat("10.67")
+// 'number'
+// Number("10.67")
+// 10.67
+// String(20)
+// '20'
+// String(false)
+// 'false'
+
+
+// string literals 
+
+let v1 = "v1";
+let v2 = 'v2';
+let v3 = `v3
+
+
+ldlksdfl;skdfl;sfkl;fsd
+kldfsjkljsdflkjfsd
+kdfsjkljdskfllkfdsjkljdfs
+
+
+
+
+
+`;
+// console.log(v1);
+// console.log(v3);
+
+const n1 = "ali";
+const n2 = "hasnain";
+const age = 45;
+
+// console.log("My name is " + n1 + " " + n2 + ", and my age is " + age);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+// console.log(`My name is ${n1} ${n2} and my age is ${age}`);
+const students = ["ali", "hasnain", "usman", 109];
+// console.log(students, 'students');
+// console.log(students[0], 'students');
+// console.log(students[1], 'students');
+// console.log(students[2], 'students');
+
+// console.log(index, 'index');
+let index = 0;
+console.log(index, 'index');
+
+index = index + 1;
+// index = index + 1;
+index++
+console.log(index, 'index');
+index = index + 2;
+
+console.log(index, 'index');
+console.log(students.length, 'students.length');
+
+for (let index = 0; index < students.length; index++) {
+    const element = students[index];
+    console.log(element, index);
+}
+// for (let index = students.length - 1; index >= 0; index--) {
+//     const element = students[index];
+//     console.log(element, index);
+
+// }
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(`My name is ${n1} ${n2} and my age is ${age} ${i}`);
+
+
+// }
+// let name = "ali";
+// console.log(name, 'name');
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(`Outer Loop ${i}`);
+//     for (let j = 0; j < 5; j++) {
+//         console.log(`Inner Loop i=${i} j=${j}`);
+//     }
+
+// }
+
+
+// let i = 0;
+// while (i < students.length) {
+//     console.log('index:', i, students[i]);
+//     i++;
+// }
+
+
+// const enteredName = prompt("Enter your name: ");
+// console.log(enteredName);
+
+
+// let j = 6;
+
+// do {
+//     console.log('index:', j);
+//     j++;
+// }
+// while (j < 5); 
